52cb873e3a110030fdef167f1d0a188fe0d9549d
Miguel Ramos López <miguelon2001b@gmail.com>
2022-03-07 11:14:15 +0100

    upload rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { SentimientoComponent } from './sentimiento/sentimiento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:BienvenidaComponent},
+  {path:'sentimiento', component:SentimientoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
