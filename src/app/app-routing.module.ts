f08d404be322d276784226a2e48d7b1ee56a057d
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2022-05-04 14:25:04 +0200

    vista dashboard, vista configuración y menu navegacion para admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:'', component:BienvenidaComponent},
-  {path:'contacto', component:ContactoComponent}
-  
+  {path:'contacto', component:ContactoComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'configuracion', component: ConfiguracionComponent}
 ];
 
 @NgModule({
